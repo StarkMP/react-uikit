0dac551a04df06174754b87a442d73086ba1e4b0
Stark <deed34@yandex.ru>
2021-10-26 19:50:26 +0300

    home#3 Update settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCSSExtactPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
@@ -9,21 +7,15 @@ module.exports = {
     bundle: path.resolve(__dirname, 'src/index.tsx'),
   },
   output: {
-    filename: 'lib.js',
+    filename: 'boxis-uikit.js',
     path: path.resolve(__dirname, 'dist'),
-    library: '@boxis/uikit',
+    library: 'boxis-uikit',
     libraryTarget: 'umd',
     globalObject: 'this',
     umdNamedDefine: true,
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-    }),
     new CleanWebpackPlugin(),
-    new MiniCSSExtactPlugin({
-      filename: '[name].[contenthash:8].css',
-    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
@@ -40,38 +32,21 @@ module.exports = {
         test: /\.js$/,
         loader: 'source-map-loader',
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCSSExtactPlugin.loader,
-          'css-loader',
-          'sass-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    'autoprefixer',
-                  ],
-                ],
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
     ],
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3000,
-    watchContentBase: true,
-    progress: true,
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
   },
 };
